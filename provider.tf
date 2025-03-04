c0fb94c7ecb183a5cf997d8032ea959aa7deb3de
shubhranshu007 <44315013+shubhranshu007@users.noreply.github.com>
2025-03-04 14:35:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-12345"  # Change to a globally unique name
+  force_destroy = true  # Deletes the bucket even if it contains objects
+}
