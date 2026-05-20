59f63a8f1c235ccfbc6ed6df9f9491429bbb3fc3
shubhranshu007 <44315013+shubhranshu007@users.noreply.github.com>
2026-05-20 10:45:21 +0530

    Rename SNS topic and output to plural form

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "ap-south-1" # Change this to your region
 }
 
-resource "aws_sns_topic" "ai_sns_topic" {
+resource "aws_sns_topics" "ai_sns_topic" {
   name = "ai-sns-topic"
 }
 
-output "sns_topic_arn" {
+output "sns_topic_arns" {
   value = aws_sns_topic.ai_sns_topic.arn
 }
 
