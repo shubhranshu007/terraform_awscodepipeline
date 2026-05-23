8c6dfc24873f527229c7209d8c86cfb5c8e92971
shubhranshu007 <44315013+shubhranshu007@users.noreply.github.com>
2026-05-23 21:13:00 +0530

    Rename aws_sns_topics to aws_sns_topic

--- provider.tf
+++ provider.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "ap-south-1" # Change this to your region
 }
 
-resource "aws_sns_topics" "ai_sns_topic" {
+resource "aws_sns_topic" "ai_sns_topic" {
   name = "ai-sns-topic"
 }
 
-output "sns_topic_arns" {
+output "sns_topic_arn" {
   value = aws_sns_topic.ai_sns_topic.arn
 }
