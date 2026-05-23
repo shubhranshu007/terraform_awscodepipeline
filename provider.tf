580cc91ca3d0f60fce4c7f1abd01f6c859d57160
shubhranshu007 <44315013+shubhranshu007@users.noreply.github.com>
2026-05-23 21:47:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" # Change this to your region
 }
 
-resource "aws_sns_topic" "ai_sns_topic" {
+resource "aws_sns_topics" "ai_sns_topic" {
   name = "ai-sns-topic"
 }
 
